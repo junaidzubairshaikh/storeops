99f0a9548de0de8e37ec131130dec1d669174824
junaid.zubair <junaid.zubair@tenpearls.com>
2017-02-08 19:59:34 +0500

    passport auth

--- routes/api.php
+++ routes/api.php
@@ -14,5 +14,39 @@ use Illuminate\Http\Request;
 */
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+//Route::get('/user', function (Request $request) {
+//     echo $request;
+//    return Response::json(['hello']);
+//    return $request->user();
+
+        return response()->json(['name' => 'Abigail', 'state' => 'CA']);
+});
+
+
+//Route::get('/getToken', function (Request $request) {
+////Route::get('/user', function (Request $request) {
+////     echo $request;
+////    return Response::json(['hello']);
+////    return $request->user();
+//     return response()->json(['name' => 'Abigail', 'state' => 'CA']);
+//});
+
+
+Route::get('/gettoken',function(){
+  $credentials = array('email' => 'cse.zubair.shaikh@gmail.com', 'password' => 'secret');
+
+//        try {
+            // verify the credentials and create a token for the user
+            if (! $token=Auth::attempt($credentials)) {
+                return response()->json(['error' => 'invalid_credentials'], 401);
+            }
+
+//            $token = $user->createToken('My Token', ['place-orders'])->accessToken;
+//        }
+//         catch (Exception $e) {
+//            // something went wrong
+//            return response()->json(['error' => 'could_not_create_token'], 500);
+//        }
+         return Response::json(compact('token'));
+
 });
